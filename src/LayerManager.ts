d7b3dac0e46652845ff4eca963023fda1b14e63f
quyue <10676114@qq.com>
2026-01-16 08:10:01 +0800

    feat: 重构LayerManager类，更新层管理逻辑，使用层名称作为键存储层实例

--- src/LayerManager.ts
+++ src/LayerManager.ts
@@ -2,32 +2,48 @@ import type { LayerManger } from './types/index.d.ts';
 import type { BaseMarkerLayerClass } from './layers/index.ts';
 
 type LayerIns = InstanceType<BaseMarkerLayerClass>;
-export class LayerManager implements LayerManger<BaseMarkerLayerClass> {
-  layers: WeakMap<LayerIns, LayerIns> = new WeakMap(); //组合模式
+export class LayerManager {
+  layers: Map<string, LayerIns> = new Map(); //组合模式
 
   addLayer(layer: LayerIns) {
     if (this.hasLayer(layer)) {
       throw new Error(`Layer with name "${layer.layerName}" already exists`);
     }
-    this.layers.set(layer, layer);
+    this.layers.set(layer.layerName, layer);
   }
 
   removeLayer(layer: LayerIns) {
-    this.layers.delete(layer);
+    this.layers.delete(layer.layerName);
   }
 
-  show() {} //! 你怎么知道，你要隐藏或者显示某个图层
+  show(LayerName: string) {
+    let layer = this.layers.get(LayerName);
+    if (layer) {
+      layer.show();
+    }
+  }
 
-  hide() {}
+  hide(LayerName: string) {
+    let layer = this.layers.get(LayerName);
+    if (layer) {
+      layer.hide();
+    }
+  }
 
-  showAll() {}
+  showAll() {
+    this.layers.forEach(val => val.show());
+  }
 
-  hideAll() {}
+  hideAll() {
+    this.layers.forEach(val => val.hide());
+  }
 
-  reload() {}
+  reload() {
+    this.layers.forEach(val => val.reload());
+  }
 
   hasLayer(layer: LayerIns): boolean {
-    return this.layers.has(layer);
+    return this.layers.has(layer.layerName);
   }
 }
 export default LayerManager;
